55a04b23723eec23aac83145550f27c5151fc4b6
momomorain <160811973+momomorain@users.noreply.github.com>
2024-03-03 15:08:43 +0800

    add

--- src/views/shopping.vue
+++ src/views/shopping.vue
@@ -11,7 +11,7 @@ export default {
             .catch(error => {
                 console.error('123發生錯誤:', error);
             });
-        
+
     },
     data() {
         return {
@@ -24,7 +24,7 @@ export default {
 
 <template>
     <main>
-        
+
         <!-- 標題 -->
         <h1 class=" text-3xl m-3 text-white">商品列表</h1>
         <!-- 商品排列切換 -->
@@ -36,7 +36,7 @@ export default {
         <!-- 商品show -->
         <div class=" w-auto h-auto ">
             <!-- 商品小卡片 -->
-            <div class="flex gap-5 flex-wrap justify-center">
+            <!-- <div class="flex gap-5 flex-wrap justify-center">
                 <div v-for="(item, index) in shopList" :key="item.id" class=" w-1/6 h-auto bg-white border-2 rounded-lg">
                     <img class="p-1" src="../assets/image/300x300_0.png" alt="圖片">
                     <div class="p-3 h-[180px]">
@@ -59,14 +59,43 @@ export default {
                     <div class="bg-blue-950 flex justify-center rounded-b-lg p-2">
                         <img src="../assets/image/shoppingcar.svg" alt="">
                         <div class="text-white"> 加入購物車 </div>
-
-
                     </div>
                 </div>
+            </div> -->
 
-
+            <div  class="bg-white grid grid-cols-6 border-y-2 p-2 ">
+                <div class="flex justify-center">商品圖片</div>
+                <div class="flex justify-center">商品名稱</div>
+                <div class="flex justify-center">商品描述</div>
+                <div class="flex justify-center">價錢</div>
+                <div></div>
+                <div></div>
             </div>
-
+            <div v-for="(item, index) in shopList" :key="item.id" class="bg-white grid grid-cols-6">
+                <div class="flex justify-center items-center p-6">
+                    <img src="../assets/image/300x300_1.png" alt="">
+                </div>
+                <div class="flex justify-center items-center">{{ item.name}}</div>
+                <div class="flex justify-center items-center">{{ item.description}}</div>
+                <div class="flex justify-center items-center">{{ item.price}}</div>
+                <div class="flex gap-5 items-center">
+                    <div>
+                        <div class="flex  p-1 justify-center items-center">
+                            <button type="button"
+                                class="rounded-l-xl bg-white w-[20px] h-[20px] border-2 border-black text-center pb-6">-</button>
+                            <input type="number" class="w-[50px] border-2 border-black h-[28px] text-center "
+                                value="1">{{ quantity }}
+                            <button type="button"
+                                class="rounded-r-xl bg-white w-[20px] h-[20px] border-2 border-black pb-6">+</button>
+                        </div>
+                        <div class="flex  p-2 justify-center items-center bg-blue-950 rounded-3xl px-6">
+                            <img src="../assets/image/shoppingcar.svg" alt="">
+                            <div class="text-white b"> 加入購物車 </div>
+                        </div>
+                    </div>
+                </div>
+                <div></div>
+            </div>
         </div>
 
 
